d85508b10d9c63dd2ec93aed3ebc6410a3630f44
Endryus <endryush@gmail.com>
2024-07-26 18:04:34 -0300

    playing with cypress

--- cypress/e2e/automation.cy.js
+++ cypress/e2e/automation.cy.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'https://automationexercise.com'
-
 describe('starting some tests', () => {
-  it ('acess home page of automation exercise', () => {
-    cy.visit(BASE_URL)
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('acess home page of automation exercise', () => {
     cy.contains('Automation')
     cy.get('h1').contains('Automation')
 
@@ -10,21 +10,38 @@ describe('starting some tests', () => {
   })
 
   it('Testing invalid login', () => {
-    cy.visit(BASE_URL)
-    cy.get('div.shop-menu').contains('Login').click()
+    cy.get('div.shop-menu').contains('Login').should('have.attr', 'href', '/login').click()
     
-    cy.contains('Login to your account')
+    cy.contains('Login to your account').should('be.visible')
 
-    cy.get('[data-qa="login-email"]').type('invalid@email.com')
-    cy.get('[data-qa="login-password"]').type('invalidpassword')
+    cy.get('[data-qa="login-email"]')
+      .type('invalid@email.com')
+      .should('have.value', 'invalid@email.com')
+      .and('be.visible')
+      .and('have.attr', 'placeholder', 'Email Address')
+      .and('have.prop', 'required')
 
-    cy.get('[data-qa="login-button"]').contains('Login').click()
+    cy.get('[data-qa="login-password"]')
+      .type('invalidpassword')
+      .should('not.have.value', '')
+      .and('have.attr', 'placeholder', 'Password')
+      .and('have.prop', 'required')
+
+    cy.get('[data-qa="login-button"]').as('btnLogin').should(($button) => { // can be then instead of should // '$' because its DOM element
+      expect($button).to.have.text('Login')
+      expect($button).to.contain('Login')
+      expect($button).to.be.visible
+      expect($button).to.have.attr('type', 'submit')
+      expect($button).to.have.class('btn')
+
+      // cy.wrap($button).click() // if it has then() and not should() above, use this to click on an element 
+    })
+    cy.get('@btnLogin').click() // get alias as()
 
     cy.contains('Your email or password is incorrect!')
   })
 
   it('products items', () => {
-    cy.visit(BASE_URL)
     cy.get('.features_items')
     cy.get('.features_items').children().first()
     cy.get('.features_items').children().last()
@@ -33,9 +50,30 @@ describe('starting some tests', () => {
     cy.get('[data-product-id="2"]')
   })
 
-  it.only('Set an item in cart and keep buying', () => {
-    cy.visit(BASE_URL)
+  it('Set an item in cart and keep buying', () => {
     cy.get('.features_items').children().eq(2).contains('Add to cart').click()
     cy.get('#cartModal').contains('Continue Shopping').click()
+  })
+
+  it('Acessing product page using intercept', () => { // only hear endpoint
+    cy.intercept('GET', 'products').as('getProducts')
+
+    cy.get('.navbar-nav').contains('Products').then(($btn) => {
+      cy.wrap($btn).click()
+    })
+
+    cy.wait('@getProducts').its('response.statusCode').should('eq', 200)
+  })
+
+  it('GET products using request', () => { // do request by itself
+    cy.request('GET', 'api/productsList').should((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).not.to.be.empty
+      
+      const bodyResponse = JSON.parse(response.body)
+      expect(bodyResponse).to.have.property('products')
+      expect(bodyResponse.products).to.be.an('array')
+      expect(bodyResponse.products).to.have.length.above(1)
+    })
   })
 })
